f63a5e4e3cc50d563990c45b362cc27541693c3b
alex-fossa-204 <shift.kite11@yandex.ru>
2023-10-02 17:33:33 +0300

    intermediate profile result

--- src/pages/workspace/domain/Profile.jsx
+++ src/pages/workspace/domain/Profile.jsx
@@ -10,8 +10,8 @@ const Profile = () => {
     return (
         <WorkspacePage>
             <div className='grid grid-rows-3 grid-flow-col gap-4'>
-                <div className='col-span-1 row-span-3 flex flex-col justify-center bg-blue-50 drop-shadow-xl'>
-                    <div className="grid grid-cols-1 justify-items-center">
+                <div className='col-span-1 row-span-3 flex flex-col justify-start bg-blue-50 drop-shadow-xl p-4'>
+                    <div className="grid grid-cols-1 justify-items-center bg-darkBlue text-white p-2 mx-5 rounded-lg">
                         <div className='p-2'>
                             <img src={avatar2} className="rounded-full w-36 h-36" alt="" />
                         </div>
@@ -37,7 +37,9 @@ const Profile = () => {
                     </div>
                 </div>
                 <div className='col-span-2 row-span-2 bg-blue-50 drop-shadow-xl p-4'>
-                    <p className='text-xl font-bold'>Персональные данные пользователя</p>
+                    <div className='bg-darkBlue p-2 rounded-lg'>
+                        <p className='text-xl font-bold text-white'>Персональные данные пользователя</p>
+                    </div>
                     <div className='grid grid-cols-4 mt-5 p-2 border border-blue-600'>
                         <div className='flex justify-start text-lg gap-5'>
                             <div className='font-bold'>Фамилия:</div>
@@ -93,8 +95,42 @@ const Profile = () => {
                     </div>
                 </div>
                 <div className='col-span-2 row-span-1 bg-blue-50 drop-shadow-xl p-4'>
-                    <div>
-                        Данные аккаунта пользователя
+                    <div className='bg-darkBlue p-2 rounded-lg'>
+                        <p className='text-xl font-bold text-white'>Данные аккаунта</p>
+                    </div>
+                    <div className='grid grid-cols-3 mt-5 p-2 border border-blue-600'>
+                        <div className='flex justify-start text-lg gap-5'>
+                            <div className='font-bold'>Логин:</div>
+                            <div className='font-medium'>{currentUser.authorityInfo.login}</div>
+                        </div>
+                        <div className='flex justify-start text-lg gap-5'>
+                            <div className='font-bold'>Почта:</div>
+                            <div className='font-medium'>{currentUser.authorityInfo.email}</div>
+                        </div>
+                        <div className='flex justify-end'>
+                            <button className={`text-gray-100 rounded-lg bg-yellow-400 p-3`} onClick={() => { }}>
+                                <AiFillEdit className='w-4 h-4' />
+                            </button>
+                        </div>
+                    </div>
+                    <div className='grid grid-cols-3 mt-5 p-2 border border-blue-600'>
+                        <div className='flex justify-start text-lg gap-5'>
+                            <div className='font-bold'>Роль в системе:</div>
+                            <div className='font-medium'>{currentUser.authorityInfo.role.roleName}</div>
+                        </div>
+                        <div className='flex justify-start text-lg gap-5'>
+                            <div className='font-bold'>Привелегии:</div>
+                            <div className='flex flex-row gap-2'>{currentUser.authorityInfo.role.privileges.map(privilege => {
+                                return (
+                                    <div className='font-medium '>{privilege}</div>
+                                );
+                            })}</div>
+                        </div>
+                        <div className='flex justify-end'>
+                            <button className={`text-gray-100 rounded-lg bg-yellow-400 p-3`} onClick={() => { }}>
+                                <AiFillEdit className='w-4 h-4' />
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
